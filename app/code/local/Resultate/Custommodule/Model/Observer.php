018979f613280c17332969e285774e6a5234441d
leuterio <jeffersonmoraes@live.com>
2017-10-31 12:00:46 -0200

    setting selected variables to module

--- app/code/local/Resultate/Custommodule/Model/Observer.php
+++ app/code/local/Resultate/Custommodule/Model/Observer.php
@@ -11,27 +11,46 @@ class Resultate_Custommodule_Model_Observer{
 
   public function cancelUnpaidOrders(){
 
-    $orderCollection = Mage::getResourceModel('sales/order_collection');
 
-    $orderCollection //estado = pagamento pendente
-      ->addFieldToFilter('state', 'processing')//pedidos com o estado de pagamento pendente
-      ->addFieldToFilter('created_at', array(
-      'lt' =>  new Zend_Db_Expr("DATE_ADD('".now()."', INTERVAL -'7' DAY)"))) //pega os pedidos criados a T atrás
-      ->getSelect()->order('entity_id')->limit(10) //limite de itens por job
-    ;
 
-    $orders=""; //inicializa pedidos
+    $selectedStatusUnformated = Mage::getStoreConfig('expiry_options/expiry_selection/expiry_status_in');
+    $selectedStatusIn = "'".$selectedStatusUnformated."'";
+    $selectedStatusOut = Mage::getStoreConfig('expiry_options/expiry_selection/expiry_status_out');
+    $selectedDays = Mage::getStoreConfig('expiry_options/expiry_selection/expiry_day');
+    $selectedTimeUnformated = Mage::getStoreConfig('expiry_options/expiry_selection/expiry_time');
+    $selectedTime = str_replace(',', ':', $selectedTimeUnformated); //formatando a hora para hh:mm:ss
+    $selectedIsEnabled = Mage::getStoreConfig('expiry_options/expiry_selection/expiry_enable');
 
-    foreach($orderCollection->getItems() as $order){
-      $orderModel = Mage::getModel('sales/order');
-      $orderModel->load($order['entity_id']);
+    Mage::log('Status entrada: '.$selectedStatusIn);
+    Mage::log('Status saída: '.$selectedStatusOut);
+    Mage::log('Dias: '.$selectedDays);
+    Mage::log('Horas: '.$selectedTime);
+    Mage::log('Habilitado: '.$selectedIsEnabled);
 
-      if(!$orderModel->canCancel()) //verifica se é possível cancelar do estado atual
-                        continue;
+    if($selectedIsEnabled == '1'){
+      $orderCollection //estado = pagamento pendente
+        ->addFieldToFilter('status', $selectedStatusIn)//pedidos com o estado de pagamento pendente
+        ->addFieldToFilter('created_at', array(
+        'lt' =>  new Zend_Db_Expr("DATE_ADD('".now()."', INTERVAL -'$selectedDays $selectedTime'  DAY_SECOND)"))) //pega os pedidos criados a T atrás
+        ->getSelect()->order('entity_id')->limit(10) //limite de itens por job
+      ;
 
-      $orderModel->cancel(); //executa o processo de cancelamento do pedido
-      $orderModel->setStatus('canceled'); //seta o status para cancelado
-      $orderModel->save(); //salva as alterações
+      $orders=""; //inicializa pedidos
+
+      foreach($orderCollection->getItems() as $order){
+        $orderModel = Mage::getModel('sales/order');
+        $orderModel->load($order['entity_id']);
+
+        if(!$orderModel->canCancel()) //verifica se é possível cancelar do estado atual
+                          continue;
+
+        Mage::log('pode ser cancelado');
+        
+        $orderModel->cancel(); //executa o processo de cancelamento do pedido
+        $orderModel->setStatus($selectedStatusOut); //seta o novo status
+        $orderModel->save(); //salva as alterações
+      }
     }
+
   }
 }
